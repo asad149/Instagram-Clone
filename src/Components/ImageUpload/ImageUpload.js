7c727c1d374e64e66be4b730d9f5d1a11b909791
asad149 <asad.arshad.94849@gmail.com>
2020-10-22 03:32:01 +0500

    Upload image and other Stuff into firebase

--- src/Components/ImageUpload/ImageUpload.js
+++ src/Components/ImageUpload/ImageUpload.js
@@ -1,7 +1,8 @@
 import { Button } from "@material-ui/core";
 import React, { useState } from "react";
-
-const ImageUpload = () => {
+import db, { storage } from "../../firebase";
+import firebase from "firebase";
+const ImageUpload = ({ username }) => {
   const [image, setImage] = useState(null);
   const [progress, setProgress] = useState(0);
   const [caption, setCaption] = useState("");
@@ -12,9 +13,46 @@ const ImageUpload = () => {
     }
   };
 
-  const handleUpload = () => {};
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // progress function ....
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        // Error function
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        // complete function ...
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            // post image inside db
+            db.collection("posts").add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+              caption: caption,
+              imageUrl: url,
+              username: username,
+            });
+            setProgress(0);
+            setCaption("");
+            setImage(null);
+          });
+      }
+    );
+  };
   return (
     <div>
+      <progress value={progress} max="100" />
       <input
         placeholder="Enter a caption...."
         type="text"
